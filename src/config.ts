73763db4a84ce19fb0357b8fbe9bfdda14724627
godow <zhs3709@163.com>
2022-08-21 19:29:03 +0800

    fix

--- src/config.ts
+++ src/config.ts
@@ -14,23 +14,18 @@ interface IConfig {
 const config: IConfig = {
   title: "米兔 - 工具集合",
   modules: {
-    编程相关: [
-      {
-        type: "function",
-        path: "FormatJson",
-        name: "JSON格式化",
-      },
+    娱乐: [
       {
         type: "function",
         path: "ArticleGenerator",
         name: "狗屁不通文章生成器",
       },
     ],
-    娱乐: [
+    编程相关: [
       {
         type: "function",
-        path: "ArticleGenerator",
-        name: "狗屁不通文章生成器",
+        path: "FormatJson",
+        name: "JSON格式化",
       },
     ],
   },
